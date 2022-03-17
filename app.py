b1b14e503c1a8796d7ca9d5af62294486710a26f
Shivam Raina <sraina753@gmail.com>
2022-03-17 15:32:50 +0530

    proto commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from speech_to_text import automatic_speech_recognition
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "HELLO"
+
+@app.route("/speechtotext/<fileId>", methods=['GET'])
+def speechToText(fileId):
+    speechTranscript = automatic_speech_recognition(fileId)
+    return jsonify(speechTranscript)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
